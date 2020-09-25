52b868e5696293da276dd7871fa3c816c1c73036
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-09-25 19:19:39 +0300

    Add squad data

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const squad = [
+  {
+    name: 'Bernd Leno',
+    position: 'Goal Keeper',
+    nationality: 'Germany'
+  },
+  {
+    name: 'Bukayo Saka',
+    position: 'Midfielder',
+    nationality: 'England'
+  },
+  {
+    name: 'David Luiz',
+    position: 'Defender',
+    nationality: 'Brazil'
+  },
+  {
+    name: 'Pierre-Emerick Aubameyang',
+    position: 'Forward',
+    nationality: 'Gabon'
+  },
+  {
+    name: 'Nicolas Pépé',
+    position: 'Forward',
+    nationality: 'Cote D\'Ivoire'
+  }
+  
+]
